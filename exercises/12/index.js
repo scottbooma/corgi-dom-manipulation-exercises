4c5d82c20c0e687d1f5bbedfdd6ae53aa3e6a340
scottbooma <scottbooma@gmail.com>
2022-03-29 11:02:28 -0400

    Add solution 12

---
+++ exercises/12/index.js
@@ -0,0 +1,7 @@
+const queensDiv = document.querySelector(".queens")
+const ul = document.createElement("ul")
+const li = document.createElement("li")
+
+li.textContent = "Kiki"
+ul.append(li)
+queensDiv.append(ul)
