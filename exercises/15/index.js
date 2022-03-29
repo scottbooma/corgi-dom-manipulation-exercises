719ef16f282c40a375e2e077556d64da642a7e78
scottbooma <scottbooma@gmail.com>
2022-03-29 11:13:15 -0400

    Add solution 15

---
+++ exercises/15/index.js
@@ -0,0 +1,5 @@
+const body = document.querySelector("body")
+const img = document.createElement("img")
+
+img.src = "https://mymodernmet.com/wp/wp-content/uploads/2020/10/cooper-baby-corgi-dogs-8.jpg"
+body.append(img)
